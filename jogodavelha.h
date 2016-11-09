fac6c09b7bd71cc7f5384c2dabcfd6b085bda547
Luís Eduardo <luissilva7991@gmail.com>
2016-11-09 07:10:05 -0300

    Update jogodavelha.h

--- jogodavelha.h
+++ jogodavelha.h
@@ -1,7 +1,22 @@
-// Menu jogo da velha
-//jogodavelha.h
+/*   _                         _        __     __   _ _           
+    | | ___   __ _  ___     __| | __ _  \ \   / /__| | |__   __ _ 
+ _  | |/ _ \ / _` |/ _ \   / _` |/ _` |  \ \ / / _ \ | '_ \ / _` |
+| |_| | (_) | (_| | (_) | | (_| | (_| |   \ V /  __/ | | | | (_| |
+ \___/ \___/ \__, |\___/   \__,_|\__,_|    \_/ \___|_|_| |_|\__,_|
+             |___/ By: Luis Eduardo, Tiago, Luis Felipe, Danillo e Daniel.
+*/
 
+// MATRIZ - JOGO!
 int matriz[3][3];
+
+// ================================================*
+void menu();
+void startjogo();
+void atribui(int valor, int x, int y);
+void mostramatriz();
+// ================================================*
+
+// Menu jogo da velha
 void menu()
 {
 	int valormenu = 0;
@@ -24,7 +39,7 @@ void menu()
 	switch (valormenu) 
 	{
 		case 1:
-			printf("chamar funcao para jogar\n");
+			startjogo();
 			break;
 		case 2:
 			printf("print Rank\n");
@@ -45,16 +60,36 @@ void startjogo() {
 	// var jogadores
 	char jogador1[10];
 	char jogador2[10];
+	int posx = 0;
+	int posy = 0;
+	int cont = 0;
+	int oi = 1;
 	// Nome dos jogadores
 	printf("Nome do jogador 1 para ser (X)\n");
-	scanf("%s", jogador1);
+	scanf(" %[^\n]s", jogador1);
 	printf("Nome do jogador 1 para ser (O)\n");
-	scanf("%s", jogador2);
+	scanf(" %[^\n]s", jogador2);
 	printf("Jogador: %s ficou com (X), Jogador: %s ficou com (O)\n", jogador1, jogador2);
+	while (cont != 9){
+		cont++;
+		oi++;
+		printf("qual posicao deseja jogar?\n");
+		printf("%d\n", cont);
+			if (oi % 2 == 0){
+				scanf("%d %d", &posx, &posy);
+				(posy && posx != 1 || 2 || 3) ? (printf("erro\n")) : (printf("foi\n"));
+				atribui(1, posx, posy);
+			}
+			else {
+				scanf("%d %d", &posx, &posy);
+				(posy && posx != 1 || 2 || 3) ? (printf("erro\n")) : (printf("Foi\n"));
+				atribui(2, posx, posy);
+			}
+	}
 }
 
 
-// ATRIBUIR VALOR A MATRIZ & POSISAO DA MATRIZ
+// ATRIBUIR VALOR A MATRIZ & POSICAO DA MATRIZ
 void atribui(int valor, int x, int y)
 {
 	// x-1 e y-1 pq começa em [0] [0]
@@ -62,7 +97,7 @@ void atribui(int valor, int x, int y)
 }
 
 
-// MOSTRA O A MATRIZ, JOGO DA VELHA...
+// MOSTRA A MATRIZ, JOGO DA VELHA...
 /*
 * Falta colocar as | e ___ pra ficar bonito...
 */
