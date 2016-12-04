84049492fb21bd6905601f19c887ca0fa7dfa248
Luís Eduardo <luissilva7991@gmail.com>
2016-12-04 12:07:52 -0300

    Update jogodavelha.h

--- jogodavelha.h
+++ jogodavelha.h
@@ -23,21 +23,9 @@ int jogador = 0;
 //Barra de titulo
 int barra_titulo = 1;
 
-//Menu
-int valormenu = 0;
-
-//ira atribuir valor a typeenum
-
-int opcao;
-
-//enum utilizada para o menu do jogo , =1 para não começar com 0
-typedef enum {JOGAR=1, RANKING, CREDITOS , SAIR} ENUM_OPC;
-
-
 // ================================================*
 void nome_jogo();
 void barra_de_titulo();
-void menu();
 void startjogo();
 void creditos();
 void atribui(int valor, int linha, int coluna, int player);
@@ -49,59 +37,6 @@ void limpar();
 void jogar_novamente();
 // ================================================*
 
-void nome_jogo(){
-    system ("title Jogo da Velha");
-}
-
-void barra_de_titulo(){
-	printf("     _                         _        __     __   _ _           \n");
-	printf("    | | ___   __ _  ___     __| | __ _  \\ \\   / /__| | |__   __ _ \n");
-	printf(" _  | |/ _ \\ / _` |/ _ \\   / _` |/ _` |  \\ \\ / / _ \\ | '_ \\ / _` |\n");
-	printf("| |_| | (_) | (_| | (_) | | (_| | (_| |   \\ V /  __/ | | | | (_| |\n");
-	printf(" \\___/ \\___/ \\__, |\\___/   \\__,_|\\__,_|    \\_/ \\___|_|_| |_|\\__,_|\n");
-	printf("             |___/\n\n");
-}
-
-
-void menu()
-{
-	
-	system("clear || cls");
-			// PRINT MENU
-	barra_de_titulo();
-	printf("* ---------------------------  MENU  ---------------------------- *\n\n");
-	printf("\t[1] - JOGAR\n\t[2] - RANKING\n\t[3] - CREDITOS\n\t[4] - SAIR\n");
-	printf("\n->  ");
-	
-	scanf("%d", &valormenu);
-	
-	//opcao vai receber o valor da enum
-    do {	
-		switch(valormenu) {
-			case 1 :
-				opcao = JOGAR;
-				break;
-			case 2 :
-				opcao = RANKING;
-				break;
-			case 3 :
-				opcao = CREDITOS;
-				break;
-				//nao funcional ainda falta ter a opcao de redirencionamento para o menu
-			case 4 :
-				opcao = SAIR;
-				break;
-			default :
-				valormenu = 0;
-				printf("VALOR INVALIDO! \n");
-				printf("\t[1] - JOGAR\n\t[2] - RANKING\n\t[3] - CREDITOS\n\t[4] - SAIR\n");
-				scanf("%d", &valormenu);
-				//nao funcional ainda falta ter a opcao de redirencionamento para o menu
-		}	
-	} while(valormenu<1 & valormenu>4);		
- 	
-}
-
 // INICIAR O JOGO, NOMES DOS 2 JOGADORES e quem vai ser "X" e "O".
 void startjogo() {
 	// Linha e Coluna, j & i;
@@ -110,7 +45,7 @@ void startjogo() {
 
 	/*
 	* Nome do jogador 1 e 2
-	* " %[^\n]s" serve para não pular a linha quando tiver nome com espaço.
+	* " %[^\n]s" serve para não pular a linha quando se tem nome com espaço.
 	*/
 	barra_de_titulo();
 	printf("Nome do jogador 1 para ser (X)\n");
@@ -136,7 +71,7 @@ void startjogo() {
 	printf("Jogador: %s ficou com (X), Jogador: %s ficou com (O)\n", jogador1, jogador2);
 	sleep(2);
     system("clear || cls");
-	// 9 jogador o maximo de vezes que pode ser jogado, 3x3 = 9. Matriz só cabe 9 elementos;
+	// 9 jogadas... o maximo de vezes que pode ser jogado, 3x3 = 9. Matriz só cabe 9 elementos;
 	for (jogador = 0; jogador < 9; jogador++)// valor 3 para ser + rapido
 	{
 	    if(barra_titulo == 1){
@@ -144,9 +79,8 @@ void startjogo() {
 	    }
 		printf("\t_____________________\n");
 		mostramatriz();
-		// temporario só pra dizer qual vez tá;
 		/****************************************************
-		*						REMOVER						*
+		*													*
 		* Foi colocado valor 1 e 5 para melhorar a contagem *
 		* 1*3 = 3, 5x3 = 15, como tava ia ficar 2*3 = 9,	*
 		* porem podia ter um 1+2 = 3.						*
@@ -215,21 +149,6 @@ void startjogo() {
 		//Empate seriam os valores 11 e 7 (2 O e 1 X ou 2 X e 1 O)!
 	}
 }
-//CREDITOS
-void creditos(){
-	system("clear || cls");
-	barra_de_titulo();
-
-	printf("* -------------------------  CREDITOS  -------------------------- *\n\n");
-	printf("Este jogo e o resultado do projeto do terceiro estagio do grupo A\n");
-	printf("da turma do P2 de Ciencia da Computacao - Noite, alunos do professor\n");
-	printf("Renato Atouguia Leite. Grupo este formado pelos alunos: Daniel Nathan,\n");
-	printf("Danillo Rodrigues, Luis Eduardo, Luis Felipe e Tiago David.\n\n");
-	printf("O conceito deste jogo e fazer com que o jogador faca todos os seus\n");
-	printf("movimentos usando apenas os comandos do teclado.\n\n");
-	printf("[1] VOLTAR AO MENU\t[2] SAIR\n\n");
-	printf("-> ");
-}
 
 /*
 * Função criada para atribuir valores a matriz(jogo da velha)
@@ -246,7 +165,7 @@ void atribui(int valor, int linha, int coluna, int player)
 
 
 /*
-* Função criada para mostrar a matriz(Jogo da Velha).
+* Função criada para imprimir a matriz(Jogo da Velha) na tela.
 */
 void mostramatriz()
 {
@@ -259,14 +178,26 @@ void mostramatriz()
 		printf("\n");
 		for ( coluna=0; coluna<3; coluna++)
 		{
+		    // Onde tiver valor zero na matriz mostrar espaço em branco
 		    if(matriz[linha][coluna] == 0)
+		    {
                 printf("\t|   |");
-		    else{
+		    }
+		    else
+		    {
+                // Onde tiver valor 1 na matriz mostrar X(Jogador 1)
                 if(matriz[linha][coluna] == 1)
+                {
                     printf("\t| X |");
+                }
                 else
+                {
+                	//Onde tiver valor 5 mostrar O(Bola, Jogador 2)
                     if(matriz[linha][coluna] == 5)
+                    {
                         printf("\t| O |");
+                    }
+                }
 		    }
   		}
 	}
@@ -274,17 +205,10 @@ void mostramatriz()
 }
 
 /*
-* Função criada para mostrar quantas vezes cada jogador jogou,
-* Pode ser aprimorada no futuro ou colocando um printf no main(jogo.c).
+* Verifica todas as posições para uma vitoria 
+* caso tenha 3 pocições em sequencia
+* com soma igual a 3 ou 15, temos um ganhador.
 */
-void quantasvezesjogou(){
-	printf("\n - %s jogou %d vezes. \n - %s jogou %d vezes. \n", jogador1, contjogador1, jogador2, contjogador2);
-}
-
-/*
-* Verifica se o jogador conseguiu a pontução 3 ou 15 para vitoria
-*/
-
 void verificavitoria(int valor) {
 	verificavitorias(valor, 1, 1, 1, 2, 1, 3);
 	verificavitorias(valor, 2, 1, 2, 2, 2, 3);
@@ -295,17 +219,29 @@ void verificavitoria(int valor) {
 	verificavitorias(valor, 1, 2, 2, 2, 3, 2);
 	verificavitorias(valor, 1, 3, 2, 3, 3, 3);
 	verificavitorias(valor, 1, 2, 2, 2, 3, 2);
-
 }
+
+/*
+* Verifica se tem uma sequencia de 3 posições com 15 pontos ou com 3 pontos.
+* int valor = 3 ou 15 que é a soma para uma vitoria
+* int valor1, int valor2, int valor3, int valor4, int valor5, int valor6
+* São posições na Matriz 
+*/
 void verificavitorias(int valor, int valor1, int valor2, int valor3, int valor4, int valor5, int valor6) {
+	//	Se o valor de uma sequencia da matriz for == valor(3 ou 15)
+	// 	então temos um ganhador
 	if (matriz[valor1-1][valor2-1] + matriz[valor3-1][valor4-1] + matriz[valor5-1][valor6-1] == valor)
 	{
+		/*
+		* Se a vez do jogador for impar, jogador 1 ganhou
+		*/
 		if (jogador % 2 == 0)
 		{
 		    system("clear || cls");
             barra_de_titulo();
             printf("\n\t\tJogador 1 [%s] ganhou!\n", jogador1);
 		}
+		// vez do jogador for par, Jogador 2 ganhou
 		else {
 			system("clear || cls");
             barra_de_titulo();
@@ -338,14 +274,7 @@ void verificavitorias(int valor, int valor1, int valor2, int valor3, int valor4,
 		jogar_novamente();
     }
 }
-//Limpar a matriz
-void limpar(){
-	int i=0 , j=0;
-	for(i=0; i<3; i++)
-		for(j=0; j<3; j++){
-			matriz[i][j]=0;
-		}
-}
+
 //Jogar novamente ou sair do jogo
 void jogar_novamente(){
 	int i=0;
@@ -359,7 +288,7 @@ void jogar_novamente(){
 			break;
 		case 2:
 			system("clear || cls");
-			menu();
+			//menu();
 			break;
 		case 3:
 			exit(EXIT_SUCCESS);
