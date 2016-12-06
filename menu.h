9b003b5dd3fa3a460398fda1b47f5ba3dddb2113
Daniel Nathan <danielzipzip@hotmail.com>
2016-12-06 19:35:16 -0300

    correção de entradas no scanf

--- menu.h
+++ menu.h
@@ -13,8 +13,10 @@ int valormenu = 0;
 
 int opcao;
 
-//enum utilizada para o menu do jogo , =1 para não começar com 0
+//enum utilizada para o menu do jogo , =1 para nao comeca com 0, logo que tem existe opcao 0
 typedef enum {JOGAR=1, RANKING, CREDITOS , SAIR} ENUM_OPC;
+
+
 
 void menu()
 {	
@@ -25,35 +27,37 @@ void menu()
 	printf("\t[1] - JOGAR\n\t[2] - RANKING\n\t[3] - CREDITOS\n\t[4] - SAIR\n");
 	printf("\n->  ");
 	
-	scanf("%d", &valormenu);
+	do{
+		scanf("%d", &valormenu);
+		if(valormenu<1 || valormenu>4){
+		
+			printf("Valor Invalido");
+			sleep(1);
+			system("clear || cls");
+			// PRINT MENU
+			barra_de_titulo();
+			printf("* ---------------------------  MENU  ---------------------------- *\n\n");
+			printf("\t[1] - JOGAR\n\t[2] - RANKING\n\t[3] - CREDITOS\n\t[4] - SAIR\n");
+			printf("\n->  ");
+		}
+		
+	}while(valormenu<1 || valormenu>4);
 	
-	//opcao vai receber o valor da enum
-    do {	
-		switch(valormenu) {
-			case 1 :
-				opcao = JOGAR;
-				break;
-			case 2 :
-				opcao = RANKING;
-				break;
-			case 3 :
-				opcao = CREDITOS;
-				break;
-				//nao funcional ainda falta ter a opcao de redirencionamento para o menu
-			case 4 :
-				opcao = SAIR;
-				break;
-			default :
-				valormenu = 0;
-				system("clear || cls");
-				barra_de_titulo();
-				printf("* ---------------------------  MENU  ---------------------------- *\n\n");
-				printf("VALOR INVALIDO! \n");
-				printf("\t[1] - JOGAR\n\t[2] - RANKING\n\t[3] - CREDITOS\n\t[4] - SAIR\n");
-				printf("\n->  ");
-				scanf("%d", &valormenu);
-				//nao funcional ainda falta ter a opcao de redirencionamento para o menu
-		}	
-	} while(valormenu<1 & valormenu>4);					
+	//opcao vai receber o valor da enum	
+	switch(valormenu) {
+		case 1 :
+			opcao = JOGAR;
+			break;
+		case 2 :
+			opcao = RANKING;
+			break;
+		case 3 :
+			opcao = CREDITOS;
+			break;
+		case 4 :
+		opcao = SAIR;
+			break;
+	}	
+						
  	
 }
